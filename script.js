5a0efc52e5b86876254f9d0d815c6567af4618b2
theflexor <nurdanokenov@gmail.com>
2022-11-28 13:07:14 +0600

    добавление hover эфектов и поправка мелких багов

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let btn = document.querySelector('#nav-icon3')
 let burger_navbar = document.querySelector('.burger-navbar')
+
 btn.addEventListener('click', function () {
     this.classList.toggle('open')
     burger_navbar.classList.toggle('visible')
